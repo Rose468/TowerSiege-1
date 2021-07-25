c33c00fd98449319a68ecffbc12d7c93afdfaac1
Rose468 <lifenature776@gmail.com>
2021-07-25 19:18:31 +0530

    change

--- sketch.js
+++ sketch.js
@@ -1,1 +1,24 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
+function preload(){
+
+}
+
+function setup(){
+   createCanvas(1200,600);
+    //rectMode(CENTER);
+    engine = Engine.create();
+    world = engine.world;
+
+Engine.run(engine);
+
+}
+
+function draw(){
+    rectMode(CENTER)
+ background("cyan")
+   
+}
