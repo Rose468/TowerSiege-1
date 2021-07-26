c58186b2f1a344c1aad92e855358fdbe324f7c26
Rose468 <lifenature776@gmail.com>
2021-07-26 19:18:12 +0530

    c

--- Block.js
+++ Block.js
@@ -1,8 +1,28 @@
-class Block extends Baseclass {
+class Block {
     constructor(x, y, width, height){
-      super(x,y,width,height);
-      //this.image = loadImage("sprites/wood1.png");
+      var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+   // this.image = loadImage("sprites/base.png");
+    World.add(world, this.body);
+  }
+
+  display(){
+  var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    rectMode(CENTER);
+    rect( 0, 0, this.width, this.height);
+    pop();
+  }
+      
     }
   
-  };
+  
   
--- sketch.js
+++ sketch.js
@@ -17,7 +17,23 @@ function setup(){
 
  ground = new Ground(600,height,1200,20);
  stand = new Ground(490,450,240,10);
-block1 = new Block(330,235,30,40);
+block1 = new Block(430,295,30,40);
+block2 = new Block(460,305,30,40);
+block3 = new Block(400,320,30,40);
+block4 = new Block(490,340,30,40);
+block5 = new Block(520,360,30,40);
+block6 = new Block(550,380,30,40);
+block7 = new Block(580,400,30,40);
+//2nd row
+block8 = new Block(430,380,30,40);
+block9 = new Block(460,383,30,40);
+block10 = new Block(490,388,30,40);
+block11 = new Block(520,384,30,40);
+block12= new Block(550,390,30,40)
+//3rd row
+block13 = new Block(460,340,30,40);
+block14 = new Block(490,360,30,40);
+block15 = new Block(520,340,30,40);
 
 Engine.run(engine);
 
@@ -26,9 +42,23 @@ Engine.run(engine);
 function draw(){
     rectMode(CENTER)
  background("cyan")
- text(mouseX + "," + mouseY,30,40)
+ text(mouseX + "," + mouseY,30,40);
    Engine.update(engine);
    ground.display();
    stand.display();
    block1.display();
+   block2.display();
+   block3.display();
+   block4.display();
+   block5.display();
+   block6.display();
+   block7.display();
+   block8.display();
+   block9.display();
+   block10.display();
+   block11.display();
+   block12.display();
+   block13.display();
+   block14.display();
+  
 }
